7be1fe1aaa60429fbae5bfa0312d63a75f37caee
poplicodes <62434619+poplicodes@users.noreply.github.com>
2020-07-07 19:01:37 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,29 @@
+function enterSite(){
+    console.log("A gaye");
+    $('#top').css("visibility","visible");
+    $('#top').css("opacity","1");
+    $('#click').css("transform","translate(0,-200vh)");
+    $('#content').css("transform","translate(0)");
+    $('#content').css("visibility","visible");
+    $('#content').css("overflow","auto")
+    $('#content').css("transform","translate(0)")
+    $('#transparent').css("transform","translate(0)")
+    $('#content').css("opacity","1");
+    $('#vid').css("filter","blur(10px)");
+}
+
+function audio(){
+
+    let audio =new Audio("/Users/manavpopli/Desktop/programs/birthday/original/media/Kasoor-Prateek Kuhad-VlcMusic.CoM.mp3");
+
+    audio.play();
+    audio.volume=0.1;
+    
+}
+
+function pausevid(){
+
+    let myvideo =document.getElementById("vid");
+    myvideo.pause();
+}
+
